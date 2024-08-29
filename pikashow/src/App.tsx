e3f368ba31aef19af8c73bdeabad25e29ff155dc
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-29 15:48:26 +0530

    implement context provider

--- pikashow/src/App.tsx
+++ pikashow/src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import { MovieProvider } from './context/movie-context';
 
 
 
 function App() {
   return (
+    <MovieProvider>
    <RouterProvider router={router}/>
+    </MovieProvider>
  
   );
 }
