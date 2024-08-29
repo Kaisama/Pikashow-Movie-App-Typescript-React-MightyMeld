29244e8aa56428eeeecf8f271c31154f0681c9af
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-29 15:09:21 +0530

    all movies initial

---
+++ pikashow/src/components/Movie-list/MovieList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface MovieListProps{
+    recommendList:any
+}
+
+const MovieList = ({recommendList}:any) => {
+  return (
+    <div>MovieTrendingList</div>
+  )
+}
+
+export default MovieList
