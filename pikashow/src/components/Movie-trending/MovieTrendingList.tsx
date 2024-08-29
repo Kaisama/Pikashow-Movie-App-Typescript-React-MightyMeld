c53545b895fd7ebc278f4b90f872943396f501a3
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-29 15:08:58 +0530

    trending list for movie

---
+++ pikashow/src/components/Movie-trending/MovieTrendingList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface MovieTrendingListProps{
+    trendingList:any
+}
+
+const MovieTrendingList = ({trendingList}:any) => {
+  return (
+    <div>MovieTrendingList</div>
+  )
+}
+
+export default MovieTrendingList
