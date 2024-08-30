4094238e56c74d4a049009efbf1a3223f08c9e4f
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-30 16:27:13 +0530

    all trending movie render here

--- pikashow/src/components/Movie-trending/MovieTrendingList.tsx
+++ pikashow/src/components/Movie-trending/MovieTrendingList.tsx
@@ -1,12 +1,24 @@
+import { Box, Grid, Paper } from '@mui/material'
 import React from 'react'
+import { MovieDataType } from '../../assets/data'
+import MovieTrendingCard from '../MovieCard/MovieTrendingCard';
 
 interface MovieTrendingListProps{
-    trendingList:any
+    trendingList:MovieDataType[];
 }
 
-const MovieTrendingList = ({trendingList}:any) => {
+const MovieTrendingList = ({trendingList}:MovieTrendingListProps) => {
   return (
-    <div>MovieTrendingList</div>
+    <Box sx={{display:"flex", gap:2 , overflowX:'scroll'}}>
+        {trendingList.map((movie)=>(
+            <Grid item key={movie.id}>
+                <Paper elevation={0} sx={{backgroundColor:"transparent"}}>
+                    <MovieTrendingCard movie={movie}/>
+                </Paper>
+            </Grid>
+        ))}
+
+    </Box>
   )
 }
 
