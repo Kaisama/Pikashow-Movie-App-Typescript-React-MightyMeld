abd40ce26c121a0c4e9523089d3178afac6cd207
Akshat Bisht <bishtakshat64@gmail.com>
2024-09-02 15:14:09 +0530

    show all bookmarks

--- pikashow/src/pages/bookmark/Bookmark.tsx
+++ pikashow/src/pages/bookmark/Bookmark.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
-
+import React, { SetStateAction, useContext, useState } from 'react'
+import { MovieDataType } from '../../assets/data';
+import { MovieContext } from '../../context/movie-context';
+import Layouts from '../../Layouts/Layouts';
+import { Box, InputAdornment, InputBase, Paper, Typography } from '@mui/material';
+import MovieTrendingList from '../../components/Movie-trending/MovieTrendingList';
+import MovieList from '../../components/Movie-list/MovieList';
+import SearchIcon from '../../assets/icons/icon-search.svg';
 const Bookmark = () => {
+  const [search, setSearch] = useState("");
+  const[searchList,setSearchList]=useState<MovieDataType[]>([]);
+  const {state}=useContext(MovieContext);
+  const {movies}=state;
+  const bookmark= movies.filter((movie)=>movie.isBookmarked)
+  
+
+  const handleSearch = (e: { target: { value: SetStateAction<string> } }) => {
+    setSearch(e.target.value);
+    const newList=movies.filter((movie)=>movie.title.toLowerCase().includes(search.toLowerCase()))
+    setSearchList(newList);
+  };
   return (
-    <div>Bookmark</div>
-  )
+
+    <Layouts>
+    <Box>
+      <Paper
+        component="form"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          borderRadius: "default",
+          p: 1,
+          backgroundColor: "#10141f",
+          border: "none",
+        }}
+      >
+        <InputBase
+          placeholder={"Search for Movies or Tv Series "}
+          sx={{ ml: 1, flex: 1, color: "white", border: "none" }}
+          value={search}
+          onChange={handleSearch}
+          startAdornment={
+            <InputAdornment position="start">
+              <img src={SearchIcon} alt="search icon" width={20} height={20}/>
+            </InputAdornment>
+          }
+        />
+      </Paper>
+    </Box>
+    <Box py={2} px={4} >
+          {search ===""?(
+            <Box width='100%'>
+                    <Typography variant="h5" component='h1' my={6} fontWeight={400}>
+                     Bookmark
+                    </Typography>
+                    <MovieTrendingList trendingList={search === ""? bookmark: searchList}/> 
+            </Box>
+          ):(
+            <Box width='100%'>
+              <Typography>
+                  Found {searchList.length} results for '{search}'{""}
+
+              </Typography>
+              <MovieList recommendList={searchList}/>
+            </Box>
+          )}
+    </Box>
+  </Layouts>  )
+
 }
 
 export default Bookmark
+
